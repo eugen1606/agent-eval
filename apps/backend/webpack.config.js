4a1d3f9f4ce0af97689fec15e6b27a62d3909912
Eugen Draušnik <eugen.drausnik@bulbtech.com>
2026-01-21 17:04:45 +0100

    fix backend not refreshing on save

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,30 +1,5 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { composePlugins, withNx } = require('@nx/webpack');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/backend'),
-    clean: true,
-    ...(!isProduction && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    }),
-  },
-  watchOptions: {
-    ignored: ['**/node_modules/**', '**/dist/**', '**/data/**'],
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: isProduction,
-      sourceMap: !isProduction,
-    }),
-  ],
-};
+module.exports = composePlugins(withNx(), (config) => {
+  return config;
+});
